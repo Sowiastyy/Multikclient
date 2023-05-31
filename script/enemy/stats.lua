7c67eceb1473e4bd568f385f24cb8c821cc65c84
Piotr Dan <piotr.danczura77@gmail.com>
2023-05-31 17:02:10 +0200

    smallBugFix

--- script/enemy/stats.lua
+++ script/enemy/stats.lua
@@ -2,7 +2,7 @@ local stats = {}
 stats["testEnemy"] = {
     image = love.graphics.newImage("img/crang_kun.png"),
     speed = 100,
-    hp = 10,
+    hp = 200,
     width = 133,
     height = 133
 }
