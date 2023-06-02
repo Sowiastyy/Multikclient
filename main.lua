96f782ed17b2d90ccd2200d1c15a2fab9ce5e8d3
Piotr Dan <piotr.danczura77@gmail.com>
2023-06-02 15:23:40 +0200

    Iforogor

--- main.lua
+++ main.lua
@@ -2,8 +2,8 @@
 local Player = require("script.player")
 local Bullet = require("script.bullet")
 local Enemy = require("script.enemy")
---local client = require("lib.websocket").new("prosze-dziala.herokuapp.com", 80)
-local client = require("lib.websocket").new("localhost", 5001)
+local client = require("lib.websocket").new("prosze-dziala.herokuapp.com", 80)
+--local client = require("lib.websocket").new("localhost", 5001)
 print(client.socket)
 
 
@@ -16,12 +16,32 @@ local Players = {}
 local Enemies = {}
 
 
+local function createEntitiesList(inputString)
+local entitiesList = {}
+local currentEntity = ""
+local insideBrackets = false
 
-function client:onmessage(s)
-    --print(s)
-    if s:find("^YourID=") then
-        LocalPlayer.id = tonumber(string.sub(s, 8))
-    elseif s:find("^ENEMY") then
+for i = 1, #inputString do
+local char = inputString:sub(i, i)
+
+if char == "{" then
+    insideBrackets = true
+elseif char == "}" then
+    table.insert(entitiesList, currentEntity)
+    currentEntity = ""
+    insideBrackets = false
+else
+    if insideBrackets then
+    currentEntity = currentEntity .. char
+    end
+end
+end
+
+return entitiesList
+end
+print(createEntitiesList("ENTITIES[{PLAYER|3|450|500|32|1|1|1}{ENEMY|1|300|300|200|testEnemy}]"))
+local function getEntity(s)
+    if s:find("^ENEMY") then
         local newEnemyData= Enemy:new(0)
         newEnemyData:fromString(s)
         Enemies[newEnemyData.id]=newEnemyData
@@ -31,7 +51,12 @@ function client:onmessage(s)
         if newPlayerData.id then
             Players[newPlayerData.id]=newPlayerData
         end
-
+    end
+end
+function client:onmessage(s)
+    --print(s)
+    if s:find("^YourID=") then
+        LocalPlayer.id = tonumber(string.sub(s, 8))
     elseif s:find("^BULLET") then
         local newBulletData = Bullet:new(0, 0, 32, {0, 0, 0})
         newBulletData :fromString(s)
@@ -39,6 +64,11 @@ function client:onmessage(s)
             table.insert(AllyBullets, newBulletData)
         else
             table.insert(EnemyBullets, newBulletData)
+        end
+    elseif s:find("^ENTITIES") then
+        for _, value in pairs(createEntitiesList(s)) do
+            print(value)
+            getEntity(value)
         end
     end
 end
