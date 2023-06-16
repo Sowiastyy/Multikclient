00a9a4edf0886cb1f81b23e94b599d593db957e6
Piotr Dan <piotr.danczura77@gmail.com>
2023-06-16 09:27:03 +0200

    client_change

--- script/game/init.lua
+++ script/game/init.lua
@@ -13,8 +13,8 @@ local chat = require("script.chat")
 local Joystick = require("script.joystick")
 
 require("script.helpers")
---local client = require("lib.websocket").new("prosze-dziala.herokuapp.com", 80)
-local client = require("lib.websocket").new("localhost", 5001)
+local client = require("lib.websocket").new("prosze-dziala.herokuapp.com", 80)
+--local client = require("lib.websocket").new("localhost", 5001)
 
 local world = wf.newWorld(0, 0) 
 
