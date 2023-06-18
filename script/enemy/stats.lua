bdc8657d1309902337ff38642159b44491f64482
misiaell7 <michalbatiuk@gmail.com>
2023-06-18 21:26:20 +0200

    zdj

--- script/enemy/stats.lua
+++ script/enemy/stats.lua
@@ -15,4 +15,12 @@ stats["testBoss"] = {
     width = 114,
     height = 138
 }
+stats["testBoss2"] = {
+    image = love.graphics.newImage("img/cyklop/downwalk.png"),
+    speed = 50,
+    hp = 2000,
+    size = 256,
+    width = 256,
+    height = 138
+}
 return stats
