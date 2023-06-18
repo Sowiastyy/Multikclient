89970d8697c71e435ee035191e894b381252f33c
Piotr Dan <piotr.danczura77@gmail.com>
2023-06-18 22:27:22 +0200

    git

--- script/enemy/stats.lua
+++ script/enemy/stats.lua
@@ -24,4 +24,59 @@ stats["testBoss"] = {
     width = 114,
     height = 138
 }
+stats["Logger"] = {
+    image = loggerSheet,
+    draw = function (self, x, y)
+        love.graphics.draw(loggerSheet, quadTest[2], x-40, y, 0, 5, 5)
+    end,
+    hp = 125,
+    width = 80,
+    height = 85,
+    bulletCollisionOffsetY = 40,
+    bulletCollisionOffsetX = 0,
+}
+stats["CrazyLogger"] = {
+    image = loggerSheet,
+    draw = function (self, x, y)
+        love.graphics.draw(loggerSheet, quadTest[1], x-40, y, 0, 5, 5)
+    end,
+    hp = 125,
+    width = 80,
+    height = 85,
+    bulletCollisionOffsetY = 40,
+    bulletCollisionOffsetX = 0,
+}
+stats["BattleLogger"] = {
+    image = loggerSheet,
+    draw = function (self, x, y)
+        love.graphics.draw(loggerSheet, quadTest[3], x-40, y, 0, 5, 5)
+    end,
+    hp = 125,
+    width = 80,
+    height = 85,
+    bulletCollisionOffsetY = 40,
+    bulletCollisionOffsetX = 0,
+}
+stats["Squirwel"] = {
+    image = loggerSheet,
+    draw = function (self, x, y)
+        love.graphics.draw(loggerSheet, quadTest[5], x-40, y, 0, 5, 5)
+    end,
+    hp = 125,
+    width = 80,
+    height = 85,
+    bulletCollisionOffsetY = 40,
+    bulletCollisionOffsetX = 0,
+}
+stats["PimpLogger"] = {
+    image = loggerSheet,
+    draw = function (self, x, y)
+        love.graphics.draw(loggerSheet, quadTest[4], x-40, y, 0, 5, 5)
+    end,
+    hp = 450,
+    width = 80,
+    height = 85,
+    bulletCollisionOffsetY = 40,
+    bulletCollisionOffsetX = 0,
+}
 return stats
