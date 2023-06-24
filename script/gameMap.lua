250536ce3bb74aba1acc196b8156849dee9c0f92
Piotr Dan <piotr.danczura77@gmail.com>
2023-06-24 19:35:54 +0200

    sortowanie drzew i innych

--- script/gameMap.lua
+++ script/gameMap.lua
@@ -1,6 +1,15 @@
 local sti = require("lib/sti")
 
-local gameMap = sti("maps/mapa4.lua") 
+local gameMap = sti("maps/mapa4.lua")
+
+local objectsSheet = love.graphics.newImage("maps/treeCooler.png")
+local objectsQuad = {}
+
+for x = 0, objectsSheet:getWidth()/32 do
+    local quad = love.graphics.newQuad(x * 32 , 0, 32, 32, objectsSheet:getWidth(), objectsSheet:getHeight())
+    table.insert(objectsQuad, quad) -- Dodanie quada do tablicy
+end
+local objects = {}
 ---hitboxes
 ---@return table hitboxes
 function gameMap:getHitboxes()
@@ -9,6 +18,9 @@ function gameMap:getHitboxes()
         for i, lay in ipairs(gameMap.layers) do
             if lay.type == "objectgroup" then
                 for index, obj in ipairs(lay.objects) do
+                    if obj.gid then --gid oznacza ze ma image z grida (GRID ID)
+                        table.insert(objects, obj)
+                    end
                     if obj.name == "siema" then
                         table.insert(hitboxes, obj)
                     end
@@ -24,13 +36,18 @@ local function sortowanie(LocalPlayer, Enemies, Players)
     for index1, lay in ipairs(gameMap.layers) do -- gdzie type to objectgroup
         if lay.type == "objectgroup" then
             for index, value in ipairs(lay.objects) do
-                table.insert(sort, {value.y*4, "drzewo", index1})
+                --table.insert(sort, {value.y*4, "drzewo", index1})
             end
         end
     end
     for index, value in pairs(Players) do
         table.insert(sort, {value.y , "players", index})
     end
+    for index, value in pairs(objects) do
+        print("INDEX", index)
+        table.insert(sort, {value.y*4-48, "drzewo", index})
+    end
+
     for index, value in pairs(Enemies) do
         table.insert(sort, {value.y, "enemy", index})
     end
@@ -41,7 +58,13 @@ local function sortowanie(LocalPlayer, Enemies, Players)
             LocalPlayer:draw()
         elseif value[2] == "drzewo" then
             love.graphics.scale(4,4)
-            gameMap:drawLayer(gameMap.layers[value[3]])
+            print("VALUE3", value[3], value[2], value[1])
+            love.graphics.draw(
+                objectsSheet,
+                objectsQuad[objects[value[3]].gid],
+                objects[value[3]].x,
+                objects[value[3]].y-32
+            )
             love.graphics.scale(0.25,0.25)
             
         elseif value[2] == "players" then
